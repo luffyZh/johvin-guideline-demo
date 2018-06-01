ae84c91cbd950eab664fbfff19e125caaabcf6a4
zhoudeyou <zhoudy@rd.netease.com>
2018-06-01 14:07:45 +0800

    add demo

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import guideline from 'guideline';
+import './App.css';
+
+class App extends Component {
+  componentDidMount() {
+    const visitTimes = parseInt(localStorage.getItem('visitTimes'), 10) || 0;
+
+    if (visitTimes === 0 || visitTimes === '0') {
+      const guideOptions = [{
+        content: 'Welcome to use the guideline!'
+      }, {
+        element: document.getElementById('guide-node'),
+        content: 'the first guideline dom, position is top',
+        style: 'font-size: 20px; color: red;',
+        position: 'bottom'
+      }, {
+        element: document.querySelector('.footer-node'),
+        content: 'the second guideline dom, position is bottom',
+        position: 'top'
+      }];
+    
+      guideline(guideOptions, function () {
+        console.log('guideline is over');
+        localStorage.setItem('visitTimes', 1);
+      });
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">The example of guideline</h1>
+          <h2 className="App-title">Author: johvin</h2>
+          <h3 className="App-title">github:&nbsp;&nbsp;<a target='_blank' rel="noopener noreferrer" href='https://github.com/johvin/guideline'>guideline</a></h3>
+        </header>
+        <p>The guideline how to use:</p>
+        <button
+          id='guide-node'
+          className="App-btn"
+          onClick={() => this.props.history.push('/Guideline')}
+        >
+          GuideLine Auto Play Page
+        </button>
+        <div className='footer-style'>
+          <span className='footer-node' style={{ color: 'blue' }}>
+            版权所有：@author:luffyZh
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/GuideLine.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import guideline from 'guideline';
+import '../App.css';
+
+class GuideLine extends Component {
+  componentDidMount(){
+    const guideOptions = [{
+      content: 'Welcome, the new features guidelines come online !'
+    }, {
+      element: document.getElementById('header-node'),
+      content: 'all system messages and notification is here',
+      style: 'font-size: 20px; color: red;',
+      position: 'bottom'
+    }, {
+      element: document.querySelector('.footer-node'),
+      content: 'datepicker hint will tell you the datepicker\'s date range restriction',
+      position: 'top'
+    }];
+    
+    const gl = new guideline.Guideline(guideOptions, function (total, played) {
+      console.log('guideline is over, total valid ', total, 'played ', played);
+      // if you want the guideline run only once
+      // localStorage.setItem('visitTimes', 1);
+    });
+    
+    // set hint text maximum width
+    gl.hintTextMaxWidth = 800
+    // set hint text font size
+    gl.hintFontSize = 20
+    
+    // start the guideline
+    gl.play();
+    
+    // autoplay the next hint every 3 seconds
+    const timer = setInterval(function () {
+      if (gl.hasNext()) {
+        gl.next();
+      } else {
+        clearInterval(timer);
+        gl.stop();
+      }
+    }, 3000);
+  }
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <h1 id='header-node' className="App-title">The more detail for GuideLine</h1>
+          <br />
+          <h2 className="App-title">The guideline will auto play</h2>
+        </header>
+        <div className='footer-style'>
+          <span className='footer-node' style={{ color: 'blue' }}>
+            版权所有：@author:luffyZh
+          </span>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GuideLine;
